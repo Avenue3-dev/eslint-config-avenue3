cedf2d229b0c7bb62b1f590d86f5fd5937e0c16a
Rowell Heria <rowell@avenue3.com>
2024-04-26 08:13:17 +0100

    fix: add missing exntends

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,1 +1,10 @@
-module.exports = require('./index');
+module.exports = {
+  extends: ['eslint:recommended', 'airbnb-base'],
+  plugins: ['barrel-files'],
+  rules: {
+    'barrel-files/avoid-barrel-files': ['error', { amountOfExportsToConsiderModuleAsBarrel: 0 }],
+    'import/extensions': 'off',
+    'import/no-default-export': 'error',
+    'import/prefer-default-export': 'off',
+  },
+};
--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 module.exports = {
-  extends: ['eslint:recommended', 'airbnb-base'],
+  extends: ['eslint:recommended', 'airbnb-base', 'airbnb-typescript/base'],
   plugins: ['barrel-files'],
   parser: '@typescript-eslint/parser',
   rules: {
-    'barrel-files/avoid-barrel-files': [
-      'error',
-      { amountOfExportsToConsiderModuleAsBarrel: 0 },
-    ],
+    'barrel-files/avoid-barrel-files': ['error', { amountOfExportsToConsiderModuleAsBarrel: 0 }],
     'import/extensions': 'off',
     'import/no-default-export': 'error',
     'import/prefer-default-export': 'off',

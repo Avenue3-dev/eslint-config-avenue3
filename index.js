e944eef3547a8c902e78094c2cb58813546c08ea
Graham Mawji-Bull <graham@avenue3.com>
2024-05-15 15:26:11 +0100

    feat: decrease max line length to 80

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ module.exports = {
   plugins: ['barrel-files'],
   parser: '@typescript-eslint/parser',
   rules: {
-    'barrel-files/avoid-barrel-files': ['error', { amountOfExportsToConsiderModuleAsBarrel: 0 }],
+    'barrel-files/avoid-barrel-files': [
+      'error',
+      { amountOfExportsToConsiderModuleAsBarrel: 0 },
+    ],
     'import/extensions': 'off',
     'import/no-default-export': 'error',
     'import/prefer-default-export': 'off',

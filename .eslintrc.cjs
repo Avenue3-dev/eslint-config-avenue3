6fac1005a63f9fc386344e7f62300a0be9887dd8
Graham Mawji-Bull <149682495+graham-a3@users.noreply.github.com>
2024-06-07 11:23:10 +0100

    feat: decrease max line length to 80 (#4)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,10 @@ module.exports = {
   extends: ['eslint:recommended', 'airbnb-base'],
   plugins: ['barrel-files'],
   rules: {
-    'barrel-files/avoid-barrel-files': ['error', { amountOfExportsToConsiderModuleAsBarrel: 0 }],
+    'barrel-files/avoid-barrel-files': [
+      'error',
+      { amountOfExportsToConsiderModuleAsBarrel: 0 },
+    ],
     'import/extensions': 'off',
     'import/no-default-export': 'error',
     'import/prefer-default-export': 'off',
